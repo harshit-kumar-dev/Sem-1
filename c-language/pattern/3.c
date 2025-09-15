a9079740254625cea1975ed3cdf80de8c36e5b83
Harshit kumar <harshit.kumar.cg@gmail.com>
2025-09-15 12:18:35 +0530

    C assignment

---
+++ c-language/pattern/3.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    for(int i=65;i<=69;i++){
+        printf("%c ",i);
+    }
+}
---
+++ c-language/pattern/6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    for(int i=65;i<=68;i++){
+        for(int j=65;j<=68;j++){
+            printf("%c ",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
